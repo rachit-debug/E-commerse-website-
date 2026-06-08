5ec870aa4b22a26baede07150e34254dfc4df8fd
rachit-debug <thapliyalrachit8@gmail.com>
2026-06-08 20:57:53 +0530

    fix cors production issue

--- backend/index.js
+++ backend/index.js
@@ -16,11 +16,21 @@ const uploadRoutes = require('./routes/uploadRoutes');
 const cors = require('cors');
 
 // middleware
-app.use(cors({
-     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-}));
+const allowedOrigins = [
+    "https://e-commerse-website-frontend-wu4u.onrender.com",
+  "http://localhost:5173"
+];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json())
 
 const PORT = process.env.PORT || 8000;
